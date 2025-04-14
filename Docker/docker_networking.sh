e4dd3b805c324ad61d2c43e172f5382604cce53e
meermeermeer <125455345+meermeermeer@users.noreply.github.com>
2025-04-14 14:06:38 +0200

    Delete Docker directory

--- Docker/docker_networking.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-sudo docker network create -d overlay --attachable mynet
-sudo docker network connect --ip 10.0.0.61 mynet lessen-7
-sudo docker image ls
-sudo docker network inspect mynet
-
-sudo docker network disconnect mynet lessen-7
-sudo docker network rm mynet
-sudo docker network prune
